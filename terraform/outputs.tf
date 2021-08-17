d12df892b0bc924347aecc866273c210145d6bf2
Lokurte, Rahul <rahul.m.lokurte@gmail.com>
2021-08-17 16:15:20 +0530

    Remove outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
 output "profile_generator_lambda_name" {
   value = module.profile_generator_lambda.name
 }
-
-output "profile_generator_lambda_arn" {
-  value = module.profile_generator_lambda.arn
-}
-
-output "profile_generator_lambda_cloudwatch_logs_name" {
-  value = module.profile_generator_lambda.cloudwatch_logs_name
-}
